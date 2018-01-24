61204b5df03d1c5534f8f09e3fc716b672f0d079
sudan0825 <sudan0825@gmail.com>
2018-01-23 23:10:36 -0800

    get the numbers of date in a year

--- year.js
+++ year.js
@@ -14,12 +14,35 @@ month.directive('year',function(){
             
         },
         link:(scope)=>{
+            //the short expression of date
             scope.dayShort=['S','M','T','W','T','F','S'];
-            scope.fd=9;
-            //create a new date object with the first day of the year
-          scope.getfd=function(){
-              scope.fd=8;
-              return scope.fd;
+            //initial the year value with current year
+            let theyear=new Date();
+           scope.year=theyear.getFullYear();
+            
+           //when the button was clicked, go to the previous/post year
+            scope.getpreyear=function(){
+                scope.year-=1;
+            }
+            scope.getpostyear=function(){
+                scope.year+=1;
+            }
+            //check the year is leap year or not
+             if(scope.year%4==0&&scope.year%100!==0||scope.year%400==0){
+                    scope.feburaryDays=29;
+                }else{
+                     scope.feburaryDays=28;
+                }
+            //get numbers of days in a month
+                scope.dayOfMonths=[31,scope.feburaryDays,31,30,31,30,31,31,30,31,30,31];
+            //populate the callendar with the days value
+          scope.populate=function(){
+              for(let i =0; i<12;i++){
+                  for(let j=1;j<scope.dayOfMonths[i];j++){
+                      
+                      
+                  }
+              }
             
             
           }
